8e8d129ea718e438142851543072f5280ba4b8af
Lusine Sargsyan <sargsyanlq@gmail.com>
2021-02-23 23:39:52 +0400

    returns each file content to its place

--- myJS.js/getRandomNumber.js
+++ myJS.js/getRandomNumber.js
@@ -1,8 +1,8 @@
-function getRandomLetter() {
-    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    const singleLetter = letters[Math.floor(Math.random() * letters.length)];
-    return singleLetter;
+function getRandomNumber() {
+    return Math.floor(Math.random() * 100);
 }
-getRandomLetter();
-console.log(getRandomLetter());
+getRandomNumber();
+console.log(getRandomNumber());
+
+
 
